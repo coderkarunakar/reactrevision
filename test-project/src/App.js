8417743ce719fc0b3dfbb3dd841af81783e64287
Karunakar <114292518+coderkarunakar@users.noreply.github.com>
2025-03-03 08:22:21 +0530

    implementation of functional component in react

--- test-project/src/App.js
+++ test-project/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./App.css";
 //Functional component props 
 //Fucntional component means that takes props as an argument and returns jsx element
 //props are helpful to pass the data from one component to another component 
-const practice = (props) => {
-  return <div>HELLO {props.name}</div>
+const Practice = () => {
+  const [message, setMessage] = useState ("React World!");
+  const changeMessage = () => {
+    setMessage("Hello functional Compoent");
+  }
+  return (
+    <div>
+      <p>{message}</p>
+      <button type='button' onClick={changeMessage} className='functionalcompbutton'>
+        Click Me Please !
+      </button>
+    </div>
+  )
+  
 }
-export default practice;
+export default Practice;
